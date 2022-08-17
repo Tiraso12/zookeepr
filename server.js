30b7c398f2e0c1c647c92cd337275db38d661d9c
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-08-17 15:15:27 -0400

    heroku working

--- server.js
+++ server.js
@@ -42,12 +42,27 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res)=> {
     let resutls = animals;
     if (req.query){
         resutls = filterByQuery(req.query, resutls);
     }
     res.json(resutls);
+})
+
+app.get('/api/animals/:id', (req, res)=>{
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    }else{
+        res.send(404);
+    }
 })
 
 app.listen(PORT, ()=>{
